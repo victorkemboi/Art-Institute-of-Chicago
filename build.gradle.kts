a4975f37851110dd28eb1e7676b1312fe1803f8c
Vicki mes <kemboivictor@outlook.com>
2023-01-05 00:36:00 +0300

    chore: setup detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,24 @@ plugins {
     id("org.jetbrains.compose") version libs.versions.compose.multiplatform apply false
     alias(libs.plugins.detekt)
 }
+
+detekt {
+    parallel = true
+    ignoreFailures = false
+    autoCorrect = true
+    buildUponDefaultConfig = true
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+}
